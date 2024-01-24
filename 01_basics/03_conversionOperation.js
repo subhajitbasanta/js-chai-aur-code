a36c70adec2257e6e47ea06496f1761aaf44a10c
subhajitbasanta <131139339+subhajitbasanta@users.noreply.github.com>
2024-01-24 17:56:25 +0000

    Operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,6 +24,38 @@ let booleanIsLoggedIn=Boolean(isLoggedIn);
 let someNumber=33;
 
 let stringnumber = String(someNumber);
-console.log(stringnumber);
-console.log(typeof stringnumber);
+//console.log(stringnumber);
+//console.log(typeof stringnumber);
 
+
+//  ************************ Operations ***************************
+
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Hello";
+// let str2 = " Subhajit";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
