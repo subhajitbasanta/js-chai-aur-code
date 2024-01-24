1004d636097ec646e13b33c3ee42cc065abd234c
subhajitbasanta <131139339+subhajitbasanta@users.noreply.github.com>
2024-01-24 15:14:56 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";   //treat all JS code as newer version
+
+//alert(3 + 3);  //we are using nodejs,not browser
+
+// console.log(3+
+//     3);          //code readability should be high
+
+
+//number =>2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null =>standalone value
+//undifined =>
+//symbol =>unique
+
+
+//object
+
+
+console.log(typeof "subhajit");
+console.log(typeof undefined);//undifined
+console.log(typeof null);//object
