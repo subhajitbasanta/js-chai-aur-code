cdaf1edae36edc2e8cd60547bb32d7c81a6a3197
subhajitbasanta <131139339+subhajitbasanta@users.noreply.github.com>
2024-01-25 05:26:59 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,88 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null ,undefined ,Symbol ,BigInt
+
+
+/*  1.String
+
+let string1 = "Hii Subha ";
+console.log(typeof string1);
+
+*/
+
+/*   2.Number
+
+const score = 100;
+const scoreValue = 100.3;
+console.log(typeof score);
+console.log(typeof scoreValue);
+
+*/
+
+/*  3.Boolean
+
+const isLoggedIn = false;
+console.log(typeof isLoggedIn);
+
+*/
+
+/*  4.null
+
+const outsideTemp = null;
+console.log(typeof outsideTemp);  Object
+
+*/
+
+/*  5.undefined
+
+let userEmail;
+console.log(typeof userEmail);
+
+*/
+
+/*  6.Symbol
+
+const id = Symbol("123");
+const anotherId=Symbol("123");
+
+console.log(id == anotherId);
+console.log(typeof id);
+console.log(typeof anotherId);
+
+*/
+
+/*   7.BigInt
+
+const bigNumber = 3241657635n;
+console.log(typeof bigNumber);
+
+*/
+
+// Reference (Non primitive)
+
+
+// Array,Object,Functions
+
+/*  1.Array
+
+const heros = ["Subhajit", "Avijit", "Soumyajit"];
+
+*/
+
+/*   2.Object
+
+let myObj = {
+    name: "Subha",
+    age: 21,
+}
+
+*/
+
+/*   3.Functions
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+*/
+
