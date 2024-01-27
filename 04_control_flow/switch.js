cd1395e3e61a4dc1221b0f3df4a9c89c070f2c33
subhajitbasanta <131139339+subhajitbasanta@users.noreply.github.com>
2024-01-27 15:01:12 +0000

    Control flow in Js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Febuary");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("None");
+        break;
+}
